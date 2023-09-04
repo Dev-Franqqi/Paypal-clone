3ad41c619312d8e581c3a70cd84e26b970bd887e
Dev-Franqqi <esendufranklinebi@gmail.com>
2023-09-04 18:15:00 +0100

    more error fixes

--- app/components/Firstsection.tsx
+++ app/components/Firstsection.tsx
@@ -3,12 +3,14 @@ import "./Firstsect.css";
 export default function Firstsection() {
     return (
       <section className={`text-center p-3 pb-5 text-white firstsec`}>
-        <h1 className="text-5xl font-thin mt-3 mb-2">We've got you covered</h1>
+        <h1 className="text-5xl font-thin mt-3 mb-2">
+          We&apos;ve got you covered
+        </h1>
 
         <p>
           We can reimburese you if something&apos;s not right. Shop with peace
           of mind, we protect your eligible purchases. If your order doesn/t
-          arrive or isn't as the seller described, we can help you get your
+          arrive or isn&apos;t as the seller described, we can help you get your
           money back
         </p>
 
